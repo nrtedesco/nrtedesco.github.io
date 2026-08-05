5d18f9d2853bb1aacf299793008a1a0feaecfb45
Nicholas <nicholas.r.tedesco@gmail.com>
2026-08-05 16:52:37 -0400

    removes chinese locale

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ export default defineConfig({
   ...(site ? { site } : {}),
   ...(base ? { base } : {}),
   i18n: {
-    locales: ['en', 'zh-cn'],
+    locales: ['en'],
     defaultLocale: 'en',
     routing: {
       prefixDefaultLocale: false,
